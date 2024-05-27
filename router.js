b5f664bfab305c52533d6c01d45056fbc88f774d
Ferran <ferran.docencia@gmail.com>
2024-05-27 11:28:00 +0200

    html + bootstrap

--- router.js
+++ router.js
@@ -7,6 +7,10 @@ router.get('/', (req, res) => {
     res.send('Buenos días');
 });
 
+router.get('/formulario', (req, res) => {
+    
+})
+
 
 // Exportación con 'type': 'modules'
 export default router;
